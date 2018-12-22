139902d6c3e5a61e8b4c660f2b5b8eba0759f1ba
TakafumiMiki <karugami02468@gmail.com>
2018-12-22 18:20:44 +0900

    add output not noun-only words method

--- mol.py
+++ mol.py
@@ -11,19 +11,26 @@ def wakati(words):
 
 def meishi(word):
     meishi_data = []
+    n_meishi_data = []
     # meishi_datas = [i.split() for i in word if "名詞" in i]
     for i in word:
         if "名詞" in i:
             str1 = i.split()
             # str1[0]はそのままの入力文字 str1[1]はカタカナで出力
             meishi_data.append(str1[0])
-    return meishi_data 
+            
+        elif 'EOS' not in i and len(i) != 0:
+            str1 = i.split()
+            # str1[0]はそのままの入力文字 str1[1]はカタカナで出力
+            n_meishi_data.append(str1[0])
+            
+    return meishi_data,n_meishi_data
 
 def main():
     words = "リストから項目を追加したり削除したりすること"
     # words = input("文字を入力\n")
-    s = meishi(wakati(words))
-    print(s)
+    m,nm = meishi(wakati(words))
+    print(m,nm)
 
 if __name__=='__main__':
-    main() 
+    main()
