2543b0efa510389d6bcba7d22c30dcf52aec8cbd
TakafumiMiki <karugami02468@gmail.com>
2018-12-23 20:32:18 +0900

    change the input file
    文字の入力をmol.pyからw2v.pyへ移動。

--- mol.py
+++ mol.py
@@ -1,45 +1,46 @@
 import MeCab
 
-# 入力された文字に対して形態素解析を行い単語別にする
-def wakati(words):
-    # m = MeCab.Tagger()
-    m = MeCab.Tagger("-Ochasen")
-    # m = MeCab.Tagger("-Owakati")
-    # m = MeCab.Tagger("-Oyomi")
-    word = m.parse(words).split("\n")
-    return word
+class mecab():        
+    # 入力された文字に対して形態素解析を行い単語別にする
+    def __wakati(self,words):
+        # m = MeCab.Tagger()
+        m = MeCab.Tagger("-Ochasen")
+        # m = MeCab.Tagger("-Owakati")
+        # m = MeCab.Tagger("-Oyomi")
+        word = m.parse(words).split("\n")
+        return word
 
-def meishi(word):
-    meishi_data = []
-    n_meishi_data = []
-    # meishi_datas = [i.split() for i in word if "名詞" in i]
-    for i,tango in enumerate(word):
-        if "名詞" in tango:
-            str1 = tango.split()
-            # str1[0]はそのままの入力文字 str1[1]はカタカナで出力
-            meishi_data.append(i)
-            meishi_data.append(str1[0])            
-        elif 'EOS' not in tango and len(tango) != 0:
-            str1 = tango.split()
-            # str1[0]はそのままの入力文字 str1[1]はカタカナで出力
-            n_meishi_data.append(i)
-            n_meishi_data.append(str1[0])
-            
-    return meishi_data,n_meishi_data
+    def __meishi(self,word):
+        meishi_data = []
+        n_meishi_data = []
+        # meishi_datas = [i.split() for i in word if "名詞" in i]
+        for i,tango in enumerate(word):
+            if "名詞" in tango:
+                str1 = tango.split()
+                # str1[0]はそのままの入力文字 str1[1]はカタカナで出力
+                meishi_data.append(i)
+                meishi_data.append(str1[0])            
+            elif 'EOS' not in tango and len(tango) != 0:
+                str1 = tango.split()
+                # str1[0]はそのままの入力文字 str1[1]はカタカナで出力
+                n_meishi_data.append(i)
+                n_meishi_data.append(str1[0])
+                
+        return meishi_data,n_meishi_data
 
-def data():
-    words = "リストから項目を追加したり削除したりすること"
-    # words = input("文字を入力\n")
-    m,nm = meishi(wakati(words))
+    def data(self,words):
+        """
+        入力された文字に対して形態素解析の結果と文字番号が返り値として出力されます。
+        """
+        m,nm = self.__meishi(self.__wakati(words))
+        # 何単語目かの出力
+        m_index = [m[i] for i in range(0,len(m),2)]
+        nm_index = [nm[i] for i in range(0,len(nm),2)]
 
-    # 何単語目かの出力
-    m_index = [m[i] for i in range(0,len(m),2)]
-    nm_index = [nm[i] for i in range(0,len(nm),2)]
+        #単語の出力
+        m_data = [m[i] for i in range(1,len(m),2)]
+        nm_data = [nm[i] for i in range(1,len(nm),2)]
 
-    #単語の出力
-    m_data = [m[i] for i in range(1,len(m),2)]
-    nm_data = [nm[i] for i in range(1,len(nm),2)]
-
-    # print(m_data,nm_data)
-    # print(m_index,nm_index)
-    return m_data,nm_data,m_index,nm_index
+        # print(m_data,nm_data)
+        # print(m_index,nm_index)
+        return m_data,nm_data,m_index,nm_index
--- w2v.py
+++ w2v.py
@@ -1,9 +1,12 @@
 import gensim
 import mol
 
+words = "リストから項目を追加したり削除したりすること"
+# words = input("文字を入力\n")
 model = gensim.models.Word2Vec.load(r'word2vec_dict\word2vec.gensim.model')
 
-m_datas,nm_datas,m_indexes,nm_indexes = mol.data()
+mecab = mol.mecab()
+m_datas,nm_datas,m_indexes,nm_indexes = mecab.data(words)
 
 p_results = []
 bunsyo = []
@@ -15,13 +18,14 @@ for m_data in m_datas:
 # 元の文章を表現
 # 同じ表現の単語(p_result)を m_datasに置き換える
 
-# for i in range(len(m_datas + nm_datas)):
-#     if i in m_indexes:
-#         bunsyo.append(m_datas[j])
-#         j += 1
+for i in range(len(m_datas + nm_datas)):
+    if i in m_indexes:
+        bunsyo.append(m_datas[j])
+        j += 1
 
-#     elif i in nm_indexes:
-#         bunsyo.append(nm_datas[k])
-#         k += 1
+    elif i in nm_indexes:
+        bunsyo.append(nm_datas[k])
+        k += 1
 
-# print(bunsyo)
+print(bunsyo)
+
